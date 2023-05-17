a570d7b397df8893ed175597ecca19568891f3c5
Ben L <wacbleach@gmail.com>
2023-05-16 22:29:57 -0400

    use env for hosting

--- shakesearch/src/api.ts
+++ shakesearch/src/api.ts
@@ -1,18 +1,22 @@
+import { env } from "process";
+
+const host = env.REACT_APP_API_HOST || 'http://localhost:3001/';
+
 const search = async (query: string, page: number = 1, selectedWorks: string[] = []): Promise<any> => {
     const worksQuery = selectedWorks.length > 0 ? `&works=${selectedWorks.join(',')}` : '';
-    const response = await fetch(`http://localhost:3001/search?q=${query}&page=${page}${worksQuery}`);
+    const response = await fetch(`${host}search?q=${query}&page=${page}${worksQuery}`);
     const data = await response.json();
     return data;
   };
   
   const suggest = async (query: string): Promise<any> => {
-    const response = await fetch(`http://localhost:3001/suggest?q=${query}`);
+    const response = await fetch(`${host}suggest?q=${query}`);
     const data = await response.json();
     return data;
   };
 
 export const getWorks = async (): Promise<any> => {
-    const response = await fetch(`http://localhost:3001/documents`);
+    const response = await fetch(`${host}documents`);
     const data = await response.json();
     return data;
     };
